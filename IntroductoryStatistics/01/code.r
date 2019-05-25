b4fa7dd61a5b607e75fb43e388e40e384a075ce4
shumez <shumelancholie+dev@gmail.com>
2019-05-25 14:21:55 +0900

    add code.r

---
+++ IntroductoryStatistics/01/code.r
@@ -0,0 +1,10 @@
+# Filename: 	code.r
+# Project: 	/Users/shume/Developer/stat/IntroductoryStatistics/01
+# Author: 	shumez <https://github.com/shumez>
+# Created: 	2019-05-25 14:06:7
+# Modified: 	2019-05-25 14:21:43
+# -----
+# Copyright (c) 2019 shumez
+
+# 01
+# 01.01.
